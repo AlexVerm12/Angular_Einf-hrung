5ba85e14f7a3dd0e6570154a5397573419f30b26
Alexandre Vermeersch <112808930+AlexVerm12@users.noreply.github.com>
2022-12-16 17:41:57 +0100

    addes images and worked on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Angular_Einfuehrung';
+export class AppComponent implements OnInit {
+  images = ['code.jpg', 'macbook.jpg', 'uni.jpg'];
+  currentImage = 0;
+  showImage = true;
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++
+      this.currentImage = this.currentImage % this.images.length;
+        this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      },10)
+    },8000)
+  }
 }
